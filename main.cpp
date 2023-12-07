6099ebed438d3f9ab0c237d3d037c61c6d7a2595
Maxym Didukh <108902150+prumat4@users.noreply.github.com>
2023-12-07 21:57:02 +0400

    Revert "BinaryFileManager class implementation  (#1)" (#3)
    This reverts commit c8f70b97302d75670b6399e6c4a77a25be864ade.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
-#include "DataCompression.hpp"
+#include "header.hpp"
 
 int main() {
-    BinaryFileManager man("../file.dat");
-    std::vector<char> b = {static_cast<char>(0b10000111)};
-    std::bitset<9> bits{"11101110"};
-    man.writeBitSequence(bits);
-
-    auto r = man.readBitSequence(320);
-
-    for(auto a : r )
-        std::cout << static_cast<int>(static_cast<unsigned char>(a)) << " ";
-
+    BinaryFileManager man("file.dat");
+    
     return 0;
 }
