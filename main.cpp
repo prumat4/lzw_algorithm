c8f70b97302d75670b6399e6c4a77a25be864ade
Maxym Didukh <108902150+prumat4@users.noreply.github.com>
2023-12-07 21:46:07 +0400

    BinaryFileManager class implementation  (#1)
    * .
    
    * implement write byte method
    
    * save of first realisation :)
    
    * implement ReadBitSequence
    
    * .
    
    * change project structure
    
    * implement read and write methods
    
    * add ios::app flag to not override the content of .dat file while reading/writing
    
    * add bitsets  logic
    
    * move tempalte methods definition outside of a class body
    
    ---------
    
    Co-authored-by: maksymdidukh <maxym.didukh@logivations.com>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include "header.hpp"
+#include "DataCompression.hpp"
 
 int main() {
-    BinaryFileManager man("file.dat");
-    
+    BinaryFileManager man("../file.dat");
+    std::vector<char> b = {static_cast<char>(0b10000111)};
+    std::bitset<9> bits{"11101110"};
+    man.writeBitSequence(bits);
+
+    auto r = man.readBitSequence(320);
+
+    for(auto a : r )
+        std::cout << static_cast<int>(static_cast<unsigned char>(a)) << " ";
+
     return 0;
 }
