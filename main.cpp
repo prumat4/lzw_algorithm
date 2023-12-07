9f25a779f2c6b9dd8706b15a8b8d6e25e1d76bf1
maksymdidukh <maxym.didukh@logivations.com>
2023-12-07 17:19:04 +0100

    implement read and write methods

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "DataCompression.hpp"
 
 int main() {
-    BinaryFileManager man("file.dat");
-    auto r = man.ReadBitSequence(19);
+    BinaryFileManager man("../file.dat");
+    std::vector<char> b = {'a', 'b', 'c', 'p'};
+    man.WriteBitSequence(b);
+
+    auto r = man.ReadBitSequence(32);
 
     for(auto a : r )
         std::cout << static_cast<int>(static_cast<unsigned char>(a)) << " ";
--- src/DataCompression.cpp
+++ src/DataCompression.cpp
@@ -19,23 +19,24 @@ void BinaryFileManager::isValidFile(const std::fstream &file) const {
 }
 
 std::vector<char> BinaryFileManager::ReadBitSequence(size_t size) {
-    std::fstream inputFile(filePath, std::ios::in | std::ios::binary);
+    std::fstream inputFile;
+    inputFile.open(filePath, std::ios::in | std::ios::binary);
     isValidFile(inputFile);
 
     size_t sizeInBytes = getNumberOfBytes(size);
     std::vector<char> bytes(sizeInBytes);
     inputFile.read(bytes.data(), sizeInBytes);
-    inputFile.close();
 
+    inputFile.close();
     return bytes;
 }
 
-// WriteBitSequence():
-// в тебе має бути функція яка просто бере послідновсність бітів і пише їх у файл бінарний
-// як я це бачу:
-// 1. зберігаємо цу як величезний бітсет?
-// 2. біжимо по цьому бітсету і просто кожні 8 бітів пушимо в масив символів 
-// 3. потім через write записуємо  у файл?
+void BinaryFileManager::WriteBitSequence(const std::vector<char> &bits) {
+    std::fstream outputFile;
+    outputFile.open(filePath, std::ios::out | std::ios::binary);
+    isValidFile(outputFile);
 
-// які трабли:
-// 1. як знайти розмір цього початково бітсету (як з ним працювати буде точніше формулювання)
+    outputFile.write(bits.data(), bits.size());
+    
+    outputFile.close();
+}
