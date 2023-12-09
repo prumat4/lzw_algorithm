8a241d4c8a5a883b1181d6100d7a4c4e1bfb5d3c
maksymdidukh <maxym.didukh@logivations.com>
2023-12-09 21:17:04 +0100

    refactroring of LZW class

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include "ZivLempelWelchAlgorithm.hpp"
 
 int main() {
-    // BinaryFileManager man("../file.bin");
-    // auto uncompressed = man.readBitSequence(2*8);
-
-    // for(auto a : uncompressed)
-    //     std::cout << a;
-
-    // std::vector<char> u = { char(187165), char(1837165)};
-    // man.writeBytes(u);
-
     LZW first;
     first.encode(5294, "../examples/file.bin");
     first.decode(1173, "../examples/file_compressed.bin");
--- src/ZivLempelWelchAlgorithm.cpp
+++ src/ZivLempelWelchAlgorithm.cpp
@@ -1,1 +1,78 @@
 #include "ZivLempelWelchAlgorithm.hpp"
+
+void LZW::encode(size_t fileSizeInBytes, const std::string& filePath) {
+    BinaryFileManager manager;
+    auto inputData = manager.readBitSequence(8 * fileSizeInBytes, filePath);
+    
+    std::vector<int> compressed;
+    std::string w;
+
+    int dictSize = 256;
+    std::map<std::string, int> dictionary;
+    for(int i = 0; i < dictSize; ++i)
+        dictionary[std::string(1, i)] = i;
+
+    for(auto it = inputData.begin(); it != inputData.end(); ++it){
+        std::string wc = w + *it;
+
+        if(dictionary.count(wc))
+            w = wc;
+        else {
+            compressed.push_back(dictionary[w]);
+            if(dictSize < 4096)
+                dictSize++;
+            dictionary[wc] = dictSize;
+            w = std::string(1, *it);
+        }
+    }
+
+    if(!w.empty())
+        compressed.push_back(dictionary[w]);
+
+    std::string compressedFilePath = filePath.substr(0, filePath.find_last_of('.'));
+    compressedFilePath += "_compressed.bin";
+
+    manager.writeBytes(compressed, compressedFilePath);
+}
+
+void LZW::decode(size_t fileSizeInBytes, const std::string& filePath) {
+    BinaryFileManager manager;
+    auto inputData = manager.readBitSequence(8 * fileSizeInBytes, filePath);
+    
+    std::vector<char> decoded;
+    auto it = inputData.begin();
+
+    int dictSize = 256;
+    std::map<int, std::string> dictionary;
+    for (int i = 0; i < dictSize; ++i)
+        dictionary[i] = std::string(1, i);
+
+    std::string w(1, *it++);
+    decoded.push_back(w[0]);
+
+    for (; it != inputData.end(); ++it) {
+        int k = *it;
+
+        std::string entry;
+        if (dictionary.count(k)) {
+            entry = dictionary[k];
+        } else if (k == dictSize) {
+            entry = w + w[0];
+        }
+
+        for (char c : entry) {
+            decoded.push_back(c);
+        }
+
+        if(dictSize < 4096)
+            dictSize++;
+        dictionary[dictSize] = w + entry[0];
+
+        w = entry;
+    }
+
+    std::string decompressedFilePath = filePath.substr(0, filePath.find_last_of('.'));
+    decompressedFilePath += "_decompressed.bin";
+
+    manager.writeBytes(decoded, decompressedFilePath);
+}
